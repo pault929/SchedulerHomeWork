e8cad8c59aad92514a4275e6c1f22a819f6a10b0
Paul T <paultoro101@gmail.com>
2020-11-03 17:50:28 -0500

    added and fixed java, and added correct code, some css adjustment also

--- index.js
+++ index.js
@@ -1,109 +1,64 @@
-var todoInput = document.querySelector("#todo-text");
-var todoForm = document.querySelector("#todo-form");
-var todoList = document.querySelector("#todo-list");
-var todoCountSpan = document.querySelector("#todo-count");
+var date = moment().format('llll');
+document.getElementById("currentTime").append(date);
 
-var todos = [];
-
-init();
-
-function renderTodos() {
-  // Clear todoList element and update todoCountSpan
-  todoList.innerHTML = "";
-  todoCountSpan.textContent = todos.length;
-
-  // Render a new li for each todo
-  for (var i = 0; i < todos.length; i++) {
-    var todo = todos[i];
-
-    var li = document.createElement("li");
-    li.textContent = todo;
-    li.setAttribute("data-index", i);
-
-    var button = document.createElement("button");
-    button.textContent = "Save";
-
-    li.appendChild(button);
-    todoList.appendChild(li);
-  }
+function readFromLocalStorage () {
+    $(".time-block").each(function(){
+        var readId = $(this).attr("id");
+        var readTextEl = localStorage.getItem(readId);
+        if (readTextEl !== null) {
+           
+            $(this).children(".description").val(readTextEl);
+        }
+    });
 }
 
-function init() {
-  // Get stored todos from localStorage
-  // Parsing the JSON string to an object
-  var storedTodos = JSON.parse(localStorage.getItem("todos"));
-
-  // If todos were retrieved from localStorage, update the todos array to it
-  if (storedTodos !== null) {
-    todos = storedTodos;
-  }
-
-  // Render todos to the DOM
-  renderTodos();
-}
-
-function storeTodos() {
-  // Stringify and set "todos" key in localStorage to todos array
-  localStorage.setItem("todos", JSON.stringify(todos));
-}
-
-// When form is submitted...
-todoForm.addEventListener("submit", function(event) {
-  event.preventDefault();
-
-  var todoText = todoInput.value.trim();
-
-  // Return from function early if submitted todoText is blank
-  if (todoText === "") {
-    return;
-  }
-
-  // Add new todoText to todos array, clear the input
-  todos.push(todoText);
-  todoInput.value = "";
-
-  // Store updated todos in localStorage, re-render the list
-  storeTodos();
-  renderTodos();
-});
-
-// When a element inside of the todoList is clicked...
-todoList.addEventListener("click", function(event) {
-  var element = event.target;
-
-  // If that element is a button...
-  if (element.matches("button") === true) {
-    // Get its data-index value and remove the todo element from the list
-    var index = element.parentElement.getAttribute("data-index");
-    todos.splice(index, 1);
-
-    // Store updated todos in localStorage, re-render the list
-    storeTodos();
-    renderTodos();
-  }
+$(".saveBtn").click(function() {
+    var hourId = $(this).parent().attr("id");
+    var textEl = $(this).siblings(".description").val().trim();
+    localStorage.setItem(hourId, textEl);
 });
 
 
-// function updateBackground() {
-//   var 
-//     hr = (new Date()).getHours(),
-//     body = document.body,
-//     bstyle = body.style,    
-//     hello = document.querySelector(".hello");    
-//   if (hr < 10) {
-//     bstyle.backgroundColor = "yellow";
-//     bstyle.color = "black";
-//     hello.innerText = "Have a good morning";
-//   } else if (hr < 20) {
-//     bstyle.backgroundColor = "green";
-//     bstyle.color = "white";
-//     hello.innerText ="Have a good day!";
-//   } else {
-//     bstyle.backgroundColor = "black";
-//     bstyle.color = "white";
-//     hello.innerText = "Have a good night!";
-//   } 
-// }
+readFromLocalStorage();
 
-// setInterval(updateBackground, 1000 * 60);
-// updateBackground();
+
+
+var currentHour = moment().format('H');
+
+var hourElements = 
+[
+"hour-06",
+"hour-07",
+"hour-08",
+"hour-09", 
+"hour-10", 
+"hour-11", 
+"hour-12", 
+"hour-13", 
+"hour-14", 
+"hour-15", 
+"hour-16", 
+"hour-17",
+"hour-18", 
+"hour-19", 
+"hour-20", 
+"hour-21", 
+"hour-22", 
+"hour-23", 
+"hour-24",
+];
+
+
+function checkHours() {
+    for (var i = 0; i < hourElements.length; i++) {
+          if (parseInt(currentHour) < parseInt(hourElements[i].match(/(\d+)/))) {
+            $("#" + hourElements[i]).removeClass("present").addClass("future");
+        } else if (parseInt(currentHour) > parseInt(hourElements[i].match(/(\d+)/))) {
+            $("#" + hourElements[i]).removeClass("present").addClass("past");
+        } else {
+            $("#" + hourElements[i]).removeClass("present").addClass("present");
+        }}
+}
+
+checkHours();
+
